0e0169dd0011187715c67aaa7d8edf26c42e934c
Carlo Santos <carlox7@gmail.com>
2017-04-03 13:22:41 -0700

    save before deploy test

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.static('./public'));
+
+app.listen(PORT, function () {
+  console.log(`Your app is being served on localhost: ${PORT}`);
+});
