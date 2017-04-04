cc58b3954b02d0db3b3d1124ef83935319a6dd92
Carlo Santos <carlox7@gmail.com>
2017-04-03 18:13:03 -0700

    functions retrieve data for individual users

--- public/scripts/view/view.js
+++ public/scripts/view/view.js
@@ -1,12 +1,12 @@
 'use strict';
-
+var totalTime;
 //Maps total time played for each game in minutes and sums up all values and divides by 60 to get hours played
 steamData.totalTimePlayed = () => {
   let totalMinutes = steamData.all.response.games.map(function (data) {
-    console.log(data.playtime_forever);
     return data.playtime_forever;
   }).reduce((acc, val) => acc + val);
-  return totalMinutes / 60;
+  totalTime = totalMinutes / 60;
+  console.log(totalTime);
 };
 
 steamData.gameNames = () => {
