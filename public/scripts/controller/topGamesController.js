6f50cf98737005ac98ddabf2b94c644bfc09479b
Patrick Sheridan <sheridp2@gmail.com>
2017-04-05 16:31:10 -0700

    help page added

--- public/scripts/controller/topGamesController.js
+++ public/scripts/controller/topGamesController.js
@@ -7,6 +7,9 @@
     $('#about-us').hide();
     $('#game-stats').hide();
     $('#home-page').hide();
+    $('#help-page').hide()
+    $('#banner').fadeIn();
+    $('#banner-title').fadeIn();
     $('#top-games').fadeIn();
   }
   module.topGamesController = topGamesController;
