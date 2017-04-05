1546021274febc34cb5e856ad24780690ae09285
Carlo Santos <carlox7@gmail.com>
2017-04-04 17:21:25 -0700

    removed unused functions and consoles logs, added comments to code blocks

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-
 app.use(express.static('./public'));
 
 app.get('/getid', function(req,res){
   request.get(`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.KEY}&vanityurl=${req.headers.vanityurl}`)
   .end(function(err,response){
-    console.log(response.body.response.steamid);
     res.send(response.body.response.steamid);
   });
 });
@@ -19,7 +17,6 @@ app.get('/getid', function(req,res){
 app.get('/games', function(req,res){
   request.get(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.KEY}&steamid=${req.headers.steamid}&format=json&include_appinfo=1`)
   .end((err,response) => {
-    console.log(response.body);
     res.send(response.body.response);
   });
 });
