39e7e551caa992c978f0be0bb95e10a5067365f5
Patrick Sheridan <sheridp2@gmail.com>
2017-04-06 13:37:58 -0700

    leaderboard added to DB

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 'use strict';
 
+const pg = require('pg');
 const request = require('superagent');
 const express = require('express');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const conString = 'postgres://patrick:test@localhost:5432/kilovolt';
+const client = new pg.Client(conString);
+
+client.connect();
+client.on('error', function(error) {
+  console.error(error);
+});
 app.use(express.static('./public'));
 
 app.get('/getid', function(req,res){
@@ -24,3 +33,33 @@ app.get('/games', function(req,res){
 app.listen(PORT, function () {
   console.log(`Your app is being served on localhost: ${PORT}`);
 });
+
+loadDB();
+
+app.post('/leaderboard', (request, response) =>{
+  client.query(`
+  INSERT INTO leaderboard(name, time)
+   VALUES ($1, $2)`,
+  [request.headers.name, request.headers.time ]
+  )
+  .then(() => response.send('Insert complete'))
+  .catch(console.error);
+})
+
+
+function loadDB(){
+  client.query(`
+    CREATE TABLE IF NOT EXISTS leaderboard (
+      leaderboard_id SERIAL PRIMARY KEY,
+      name VARCHAR(255),
+      time VARCHAR(255)
+    )`)
+    // .then(function(){
+    //   loadSteamer();
+    // })
+    .catch(function(err){
+      console.log(err);
+    }
+  );
+}
+loadDB();
